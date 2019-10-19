78a51b916a4438aa77e88e4c8a8ded6092a3926a
rp89783 <rp89783@gmail.com>
2019-10-19 18:05:41 -0400

    Agregar archivos de proyecto.

---
+++ UtecoApp/UtecoApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UtecoApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        string user = "admin";
+        string pass = "123";
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIniciar_Clicked(object sender, EventArgs e)
+        {if (txtUsername.Text == user && txtPass.Text == pass)
+                await Navigation.PushModalAsync(new PrincipalPagina());
+
+            else
+            {
+                lblError.Text = "Usuario o clave incorrecto";
+            }
+        }
+
+        private void btncancelar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UtecoApp/UtecoApp/PrincipalPagina.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UtecoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PrincipalPagina : ContentPage
+    {
+        public PrincipalPagina()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApps_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
